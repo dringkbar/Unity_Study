c2253ce1fe75bcba082a0f0874daecdfc239b171
drinkbar <nagito12396@gmail.com>
2019-05-01 09:35:23 +0900

    ADD: Cookie Clicker

---
+++ Cookie Clicker/Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    private float _cash;
+    public float Cash
+    {
+        get { return _cash; }
+        set
+        {
+            _cash = value;
+            cashText.text = "You Have: $" + _cash.ToString("0.00");
+        }
+    }
+
+    private float _cashPerSecond;
+    public float CashPerSecond
+    {
+        get { return _cashPerSecond; }
+        set
+        {
+            _cashPerSecond = value;
+            rateText.text = "per Second: " + _cashPerSecond.ToString("0.0");
+        }
+    }
+    [Tooltip("버튼을 누를 때마다 플레이어가 벌어들이는 돈.")]
+    public float cashPerClick = 1;
+
+    [Header("Object References")]
+    public Text cashText;
+    public Text rateText;
+
+    private void Start()
+    {
+        _cash = 0;
+        _cashPerSecond = 0;
+    }
+
+    private void Update()
+    {
+        Cash += CashPerSecond * Time.deltaTime;
+    }
+
+    public void ClickButton()
+    {
+        Cash += cashPerClick;
+    }
+}
+	
+
---
+++ Cookie Clicker/Assets/Scripts/StoreItem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ItemType
+{
+    ClickPower, PerSecondIncrease
+};
+
+public class StoreItem : MonoBehaviour
+{
+    [Tooltip("이 업그레이드의 가격이 얼마인지")]
+    public int cost;
+
+    public ItemType itemType;
+
+    [Tooltip("구매한다면 얼마나 증가될것인지")]
+    public float increaseAmount;
+
+    private int qty;
+
+    public Text costText;
+    public Text qtyText;
+
+    private GameController controller;
+    private Button button;
+
+    // Use this for initialization
+    void Start()
+    {
+        qty = 0;
+        qtyText.text = "$" + cost.ToString();
+
+        button = transform.GetComponent<Button>();
+        button.onClick.AddListener(this.ButtonClicked);
+        controller = GameObject.FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        button.interactable = (controller.Cash >= cost);
+    }
+
+    public void ButtonClicked()
+    {
+        controller.Cash -= cost;
+        switch (itemType)
+        {
+            case ItemType.ClickPower:
+                controller.cashPerClick += increaseAmount;
+                break;
+            case ItemType.PerSecondIncrease:
+                controller.CashPerSecond += increaseAmount;
+                break;
+        }
+        qty++;
+        qtyText.text = qty.ToString();
+    }
+}
